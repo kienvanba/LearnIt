532019931bfd39d26533eb1359b97cf244a0d772
kienvanba <kien.vanba@gmail.com>
2017-02-13 18:36:15 +0700

    first update

---
+++ NeverNukeACountryTwice/app/src/main/java/com/example/kienvanba/nevernukeacountrytwice/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.kienvanba.nevernukeacountrytwice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.example.kienvanba.nevernukeacountrytwice.widget.WanaKanaJavaText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et = (EditText) findViewById(R.id.et);
+        WanaKanaJavaText waka = new WanaKanaJavaText(et,false);
+        waka.bind();
+    }
+}
